a8160a7a4816cca3856aa49418c26cb16f71873b
杨进玺 <jinxi.yang@bl.com>
2018-05-28 18:15:06 +0800

    coding

--- demo/src/main/java/com/yang/demo/MainActivity.java
+++ demo/src/main/java/com/yang/demo/MainActivity.java
@@ -1,18 +1,23 @@
 package com.yang.demo;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         btnOpenPreview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "" + hasSurface, Toast.LENGTH_SHORT).show();
                 if (previewing){
                     stopPreview();
                 }else {
@@ -48,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
                 hasSurface = true;
-                printDate();
             }
 
             @Override
@@ -59,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
                 hasSurface = false;
-                printDate();
             }
         });
     }
@@ -70,10 +72,10 @@ public class MainActivity extends AppCompatActivity {
 
         Camera.CameraInfo backCamera = null;
         int index = 0;
-        while (index < numberOfCameras){
+        while (index < numberOfCameras) {
             Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
             Camera.getCameraInfo(index, cameraInfo);
-            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT){
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
                 backCamera = cameraInfo;
                 break;
             }
@@ -82,18 +84,28 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "startPreview: " + index);
 
 
-        if (index < numberOfCameras){
+        if (index < numberOfCameras) {
             camera = Camera.open(index);
             try {
                 Camera.Parameters parameters = camera.getParameters();
                 List<Camera.Size> supportedPreviewSizes = parameters.getSupportedPreviewSizes();
-                if (supportedPreviewSizes != null && supportedPreviewSizes.size() > 0){
+
+                if (supportedPreviewSizes != null && supportedPreviewSizes.size() > 0) {
+                    for (Camera.Size size : supportedPreviewSizes) {
+                        Log.i(TAG, "Camera.Size: [" + size.width + "," + size.height + "]");
+                    }
                     Camera.Size size = supportedPreviewSizes.get(0);
-                    parameters.setPreviewSize(size.width, size.height);
+//                    parameters.setPreviewSize(size.width, size.height);
                 }
 
+                int width = svPreview.getWidth();
+                int height = svPreview.getHeight();
+                Log.i(TAG, "svPreview: [" + width + "," + height + "]");
+
+                Point bestPreviewSize = getBestPreviewSize(parameters, new Point(width, height));
+                parameters.setPreviewSize(bestPreviewSize.x, bestPreviewSize.y);
                 camera.setParameters(parameters);
-                camera.setDisplayOrientation(backCamera.orientation);
+                camera.setDisplayOrientation(getCameraDisplayOrientation(this, index));
                 camera.setPreviewDisplay(svPreview.getHolder());
                 previewing = true;
                 camera.startPreview();
@@ -103,23 +115,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void printDate() {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String string = format.format(new Date());
-        Log.i(TAG, "printDate: " + string + "   " + hasSurface);
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume: ");
-    }
-
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "onPause: ");
         stopPreview();
     }
 
@@ -131,4 +129,129 @@ public class MainActivity extends AppCompatActivity {
             camera = null;
         }
     }
+
+
+    /**
+     * 设置相机预览旋转方向
+     * @param context
+     * @param cameraId
+     */
+    public static int getCameraDisplayOrientation(Context context, int cameraId){
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        int rotation = windowManager.getDefaultDisplay().getRotation();
+
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+            default:
+                // Have seen this return incorrect values like -90
+                if (rotation % 90 == 0) {
+                    degrees = (360 + rotation) % 360;
+                } else {
+                    throw new IllegalArgumentException("Bad rotation: " + rotation);
+                }
+        }
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId, cameraInfo);
+        int result;
+        if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT){
+            result = (cameraInfo.orientation + degrees) % 360;
+            result = (360 - result) % 360;
+        }else {
+            result = (cameraInfo.orientation - degrees + 360) % 360;
+        }
+        return result;
+    }
+
+
+    private static final int MIN_PREVIEW_PIXELS = 480 * 320; // normal screen
+    private static final double MAX_ASPECT_DISTORTION = 0.15;
+
+    public static Point getBestPreviewSize(Camera.Parameters parameters, final Point screenResolution){
+        List<Camera.Size> rawSupportedPreviewSizes = parameters.getSupportedPreviewSizes();
+        if (rawSupportedPreviewSizes == null){
+            Camera.Size defaultSize = parameters.getPreviewSize();
+            if (defaultSize == null){
+                throw new IllegalStateException("Parameters contained no preview size!");
+            }
+            return new Point(defaultSize.width, defaultSize.height);
+        }
+
+        final float screenAspectRatio = screenResolution.x / (float)screenResolution.y;
+
+        List<Camera.Size> supportedPreviewSizes = new ArrayList<>(rawSupportedPreviewSizes);
+        Iterator<Camera.Size> iterator = supportedPreviewSizes.iterator();
+        while (iterator.hasNext()){
+            Camera.Size size = iterator.next();
+            int width = size.width;
+            int height = size.height;
+
+            //去除分辨率较小的
+            if (width * height < MIN_PREVIEW_PIXELS){
+                iterator.remove();
+                continue;
+            }
+
+            //去除宽高比和预览框宽高比差别较大的
+            boolean isCandidatePortrait = width < height;
+            int maybeFlippedWidth = isCandidatePortrait ? height : width;
+            int maybeFlippedHeight = isCandidatePortrait ? width : height;
+            double aspectRatio = maybeFlippedWidth / (double) maybeFlippedHeight;
+            double distortion = Math.abs(aspectRatio - screenAspectRatio);
+            if (distortion > MAX_ASPECT_DISTORTION) {
+                iterator.remove();
+                continue;
+            }
+
+            //正好和预览框宽高一样的，直接返回
+            if (maybeFlippedWidth == screenResolution.x && maybeFlippedHeight == screenResolution.y) {
+                Point exactPoint = new Point(width, height);
+                return exactPoint;
+            }
+        }
+
+        //按宽高比和预览宽高比最接近排序，降序
+        Collections.sort(supportedPreviewSizes, new Comparator<Camera.Size>() {
+            @Override
+            public int compare(Camera.Size o1, Camera.Size o2) {
+                int width1 = o1.width;
+                int height1 = o1.height;
+                int width2 = o2.width;
+                int height2 = o2.height;
+                float ratio1 = Math.abs((float) height1 / width1 - screenAspectRatio);
+                float ratio2 = Math.abs((float) height2 / width2 - screenAspectRatio);
+                int result = Float.compare(ratio1, ratio2);
+                if (result != 0) {
+                    return result;
+                } else {
+                    int minGap1 = Math.abs(screenResolution.x - width1) + Math.abs(screenResolution.y - height1);
+                    int minGap2 = Math.abs(screenResolution.x - width2) + Math.abs(screenResolution.y - height2);
+                    return minGap1 - minGap2;
+                }
+            }
+        });
+
+        if (supportedPreviewSizes != null && !supportedPreviewSizes.isEmpty()){
+            Camera.Size size = supportedPreviewSizes.get(0);
+            return new Point(size.width, size.height);
+        }
+
+        Camera.Size defaultSize = parameters.getPreviewSize();
+        if (defaultSize == null) {
+            throw new IllegalStateException("Parameters contained no preview size!");
+        }
+        return new Point(defaultSize.width, defaultSize.height);
+    }
+
 }
--- zxing_new/src/main/java/com/google/zxing/camera/CameraConfigurationManager.java
+++ zxing_new/src/main/java/com/google/zxing/camera/CameraConfigurationManager.java
@@ -154,8 +154,6 @@ final class CameraConfigurationManager {
             Log.w(TAG, "In camera config safe mode -- most settings will not be honored");
         }
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-
         initializeTorch(parameters, safeMode);
 
         CameraConfigurationUtils.setFocus(
